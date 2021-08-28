63abfdfc20ab97a2562e9653dc853f95d1e2f7a6
K Sam <81320998+K-Sam1005@users.noreply.github.com>
2021-08-28 17:46:15 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCcp7e9uPJGA5a45FQrksRd_xA5jYbpi-Q",
+  authDomain: "kwitter-for-project.firebaseapp.com",
+  projectId: "kwitter-for-project",
+  storageBucket: "kwitter-for-project.appspot.com",
+  messagingSenderId: "406961096625",
+  appId: "1:406961096625:web:fb2c7d73b349be51191f90"
+};
+
+const app = initializeApp(firebaseConfig);
+
